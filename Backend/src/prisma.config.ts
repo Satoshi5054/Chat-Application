99bf20f7ed97f08f6a433f7df2516096a08a7380
Satoshi5054 <shaunricky1984@gmail.com>
2026-03-17 16:33:07 +0530

    added boiler plate

---
+++ Backend/src/prisma.config.ts
@@ -0,0 +1,14 @@
+//The DATABASE_URL should be present in the .env file
+
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+});
